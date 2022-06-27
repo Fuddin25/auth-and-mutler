5183500f3b053060fdc434b432a1255699a5616b
Fuddin25 <akyuc2@gmail.com>
2022-06-28 02:54:15 +0700

    stage2Week3Day2SocketIoMiddleWares

---
+++ client/src/pages/Complain.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react'
+
+import Navbar from '../components/Navbar'
+
+// import components
+import { Container, Row, Col } from 'react-bootstrap'
+import Contact from '../components/complain/Contact'
+
+// import socket.io-client 
+import {io} from 'socket.io-client'
+
+// initial variable outside socket
+let socket
+export default function Complain() {
+    const [contact, setContact] = useState(null)
+    const [contacts, setContacts] = useState([])
+
+    const title = "Complain"
+    document.title = 'DumbMerch | ' + title
+
+    useEffect(() =>{
+        socket = io('http://localhost:5000', {
+            // code here
+        })
+        loadContact()
+
+        // code here
+
+        return () => {
+            socket.disconnect()
+        }
+    }, [])
+
+    const loadContact = () => {
+        // emit corresponding event to load admin contact
+        socket.emit("load admin contact")
+
+        // listen event to get admin contact
+        socket.on("admin contact", (data) => {
+
+             // manipulate data to add message property
+            const dataContact = {
+                ...data, 
+                message: "Click here to start message"
+            }
+            setContacts([dataContact])
+        })
+    }
+
+    // used for active style when click contact
+    const onClickContact = (data) => {
+        setContact(data)
+    }
+    return (
+        <>
+            <Navbar title={title} />
+            <Container fluid style={{height: '89.5vh'}}>
+                <Row>
+                    <Col md={3} style={{height: '89.5vh'}} className="px-3 border-end border-dark overflow-auto">
+                        <Contact dataContact={contacts}  clickContact={onClickContact} contact={contact} />
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    )
+}
---
+++ client/src/pages/ComplainAdmin.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react'
+
+import NavbarAdmin from '../components/NavbarAdmin'
+
+// import components
+import { Container, Row, Col } from 'react-bootstrap'
+import Contact from '../components/complain/Contact'
+
+// import socket.io-client 
+import {io} from 'socket.io-client'
+
+// initial variable outside socket
+let socket
+export default function ComplainAdmin() {
+    const [contact, setContact] = useState(null)
+    const [contacts, setContacts] = useState([])
+
+    const title = "Complain admin"
+    document.title = 'DumbMerch | ' + title
+
+    useEffect(() =>{
+        socket = io('http://localhost:5000', {
+            // code here
+        })
+        loadContacts()
+
+        // code here 
+        
+        return () => {
+            socket.disconnect()
+        }
+    }, [])
+
+    const loadContacts = () => {
+        socket.emit("load customer contacts")
+        socket.on("customer contacts", (data) => {
+            // filter just customers
+            let dataContacts = data.filter(item => item.status !== "admin")
+            
+            // manipulate customers to add message property
+            dataContacts = dataContacts.map((item) => ({
+                ...item,
+                message: "Click here to start message"
+            }))
+
+            setContacts(dataContacts)
+        })
+    }
+
+    // used for active style when click contact
+    const onClickContact = (data) => {
+        setContact(data)
+    }
+
+    return (
+        <>
+            <NavbarAdmin title={title} />
+            <Container fluid style={{height: '89.5vh'}}>
+                <Row>
+                    <Col md={3} style={{height: '89.5vh'}} className="px-3 border-end border-dark overflow-auto">
+                        <Contact dataContact={contacts} clickContact={onClickContact} contact={contact}/>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    )
+}
